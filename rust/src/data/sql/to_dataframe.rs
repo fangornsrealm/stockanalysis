11cf4ffaaa5f1228c54f5a25d77955ec757fdbe8
fangornsrealm <progs@fangornsrealm.eu>
2025-09-15 15:57:38 +0200

    add daily data to the database and all the supporting functions

--- rust/src/data/sql/to_dataframe.rs
+++ rust/src/data/sql/to_dataframe.rs
@@ -1,21 +1,38 @@
 //! data retrieval from database or website
 //!
-use chrono::{DateTime, NaiveDateTime};
+use chrono::{DateTime, NaiveDateTime, Timelike, Utc};
 use polars::prelude::*;
 use std::error::Error;
 //use finalytics::utils::date_utils::{round_datetime_to_day, round_datetime_to_hour, round_datetime_to_minute};
 
-/// Converts a date string in YYYY-MM-DD format to a Unix Timestamp
-pub fn to_timestamp(datetime_str: &str) -> Result<i64, Box<dyn Error>> {
+/// Converts a date string in YYYY-MM-DD format to a Unix Timestamp milliseconds since the Epoch
+pub fn str_to_timestamp(datetime_str: &str) -> Result<i64, Box<dyn Error>> {
     let datetime = NaiveDateTime::parse_from_str(datetime_str, "%Y-%m-%d %H:%M:%S")?;
-    let unix_timestamp = datetime.and_utc().timestamp();
+    let unix_timestamp = datetime.and_utc().timestamp_millis();
     Ok(unix_timestamp)
+}
+
+/// Converts a datetime to a Unix Timestamp milliseconds since the Epoch
+pub fn to_timestamp(datetime: NaiveDateTime) -> i64 {
+    datetime.and_utc().timestamp_millis()
 }
 
 /// Converts a Unix Timestamp to a date string in YYYY-MM-DD format
 pub fn to_datetime(unix_timestamp: i64) -> NaiveDateTime {
-    let datetime = DateTime::from_timestamp(unix_timestamp, 0).unwrap();
+    let datetime = DateTime::from_timestamp_millis(unix_timestamp).unwrap();
     datetime.naive_utc()
+}
+
+/// Converts a Unix Timestamp to a date string in YYYY-MM-DD format
+pub fn to_date(unix_timestamp: i64) -> NaiveDateTime {
+    let datetime = DateTime::from_timestamp_millis(unix_timestamp).unwrap();
+    datetime.date_naive().into()
+}
+
+pub fn date_to_timestamp_millis(dt: NaiveDateTime) -> i64 {
+    let datetime = dt.date().and_time(chrono::NaiveTime::default());
+    let date = datetime.and_utc();
+    date.timestamp_millis()
 }
 
 /// converts a string into a NaiveDateTime
@@ -24,22 +41,30 @@ pub fn str_to_datetime(datetime_str: &str) -> Result<NaiveDateTime, Box<dyn Erro
     Ok(datetime)
 }
 
+/// converts a vector of UNIX Timestamps to a vector of NaiveDateTime
+pub fn i64_to_datetime_vec(df: DataFrame) -> Result<Vec<NaiveDateTime>, Box<dyn Error>> {
+    let df2 = df.column("timestamp")?.i64()?
+            .into_no_null_iter().map(|x| DateTime::from_timestamp_millis(x).unwrap()
+            .naive_local()).collect::<Vec<NaiveDateTime>>();
+    Ok(df2)
+}
+
 /// Returns the Ticker OHLCV Data from the database for a given time range
 pub fn ohlcv_to_dataframe(
     sql_connection: Arc<std::sync::Mutex<rusqlite::Connection>>,
     stock_symbol: &str,
-    _start_date: NaiveDateTime,
-    _end_date: NaiveDateTime,
+    start_date: NaiveDateTime,
+    end_date: NaiveDateTime,
 ) -> Result<DataFrame, Box<dyn Error>> {
     let exchange_code = "XFRA";
     let metadata = super::metadata(sql_connection.clone(), &exchange_code, stock_symbol);
 
     let series: Vec<super::TimeSeriesData> = super::live_data(sql_connection.clone(), &metadata);
-
+    // timestamps are expected to be number of milliseconds since 1.1. 1970.
     let timestamp = series
         .iter()
-        .map(|o| to_datetime(o.datetime))
-        .collect::<Vec<NaiveDateTime>>();
+        .map(|o| o.datetime * 1000)
+        .collect::<Vec<i64>>();
 
     let open = series.iter().map(|o| o.open).collect::<Vec<f64>>();
 
@@ -68,15 +93,110 @@ pub fn ohlcv_to_dataframe(
     let df = df.filter(&mask)?;
 
     // check id any returned dates smaller than start date or greater than end date
-    // TODO: fix this sorting routine!
-    //let mask = df["timestamp"]
-    //    .datetime()?
-    //    .as_datetime_iter()
-    //    .map(|x| {
-    //        let v = x.unwrap();
-    //        start_date > v && v < end_date
-    //    })
-    //    .collect();
-    //let df = df.filter(&mask)?;
+    match i64_to_datetime_vec(df.clone()) {
+        Ok(dt) => {
+            let mask = dt.iter()
+                .map(|x| {
+                    &start_date < x && x < &end_date
+                })
+                .collect();
+            let df = df.filter(&mask)?;
+            return Ok(df);
+        }
+        Err(error) => {
+            log::error!("Unable to turn timestamps into dates to create a date filter mask! {:?}", error);
+        }
+    }
+    Ok(df)
+}
+
+/// Returns the Ticker OHLCV Data from the database for a given time range
+pub fn daily_ohlcv_to_dataframe(
+    sql_connection: Arc<std::sync::Mutex<rusqlite::Connection>>,
+    stock_symbol: &str,
+    start_date: DateTime<Utc>,
+    end_date: DateTime<Utc>,
+) -> Result<DataFrame, Box<dyn Error>> {
+    let exchange_code = "XFRA";
+    let mut metadata = super::metadata(sql_connection.clone(), &exchange_code, stock_symbol);
+    metadata.start_date = start_date;
+    metadata.end_date = end_date;
+    let series: Vec<super::TimeSeriesData> = super::timeseries(sql_connection.clone(), &metadata);
+    // timestamps are expected to be number of milliseconds since 1.1. 1970.
+    let timestamp = series
+        .iter()
+        .map(|o| date_to_timestamp_millis(to_date(o.datetime * 1000)))
+        .collect::<Vec<i64>>();
+
+    let open = series.iter().map(|o| o.open).collect::<Vec<f64>>();
+
+    let high = series.iter().map(|o| o.high).collect::<Vec<f64>>();
+
+    let low = series.iter().map(|o| o.low).collect::<Vec<f64>>();
+
+    let close = series.iter().map(|o| o.close).collect::<Vec<f64>>();
+
+    let volume = series.iter().map(|o| o.volume).collect::<Vec<f64>>();
+
+    let adjclose = series.iter().map(|o| o.close).collect::<Vec<f64>>();
+
+    let df = df!(
+        "timestamp" => &timestamp,
+        "open" => &open,
+        "high" => &high,
+        "low" => &low,
+        "close" => &close,
+        "volume" => &volume,
+        "adjclose" => &adjclose
+    )?;
+
+    // check if any adjclose values are 0.0
+    let mask = df.column("adjclose")?.as_series().unwrap().gt(0.0)?;
+    let df = df.filter(&mask)?;
+
+    Ok(df)
+}
+
+pub fn ohlcv_hourly(ohlcv: DataFrame) -> Result<DataFrame, Box<dyn Error>> {
+    let datetime = ohlcv.column("timestamp")?.i64()?.to_vec().iter().map(|x|
+        DateTime::from_timestamp_millis( x.unwrap()).unwrap().naive_local()).collect::<Vec<NaiveDateTime>>();
+    let timestamp = ohlcv.column("timestamp")?.i64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<i64>>();
+    let open = ohlcv.column("open")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    let high = ohlcv.column("high")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    let low = ohlcv.column("low")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    let close = ohlcv.column("close")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    let volume = ohlcv.column("volume")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    let adjclose = ohlcv.column("adjclose")?.f64()?.to_vec().iter().map(|x| x.unwrap()).collect::<Vec<f64>>();
+    
+    let mut timestamp_filtered = Vec::new();
+    let mut open_filtered = Vec::new();
+    let mut high_filtered = Vec::new();
+    let mut low_filtered = Vec::new();
+    let mut close_filtered = Vec::new();
+    let mut volume_filtered = Vec::new();
+    let mut adjclose_filtered = Vec::new();
+
+    for i in 0..datetime.len() {
+        let dt = datetime[i].clone();
+        if dt.minute() == 0 {
+            timestamp_filtered.push(timestamp[i]);
+            open_filtered.push(open[i]);
+            high_filtered.push(high[i]);
+            low_filtered.push(low[i]);
+            close_filtered.push(close[i]);
+            volume_filtered.push(volume[i]);
+            adjclose_filtered.push(adjclose[i]);
+        }
+    }
+
+    let df = df!(
+        "timestamp" => timestamp_filtered,
+        "open" => open_filtered,
+        "high" => high_filtered,
+        "low" => low_filtered,
+        "close" => close_filtered,
+        "volume" => volume_filtered,
+        "adjclose" => adjclose_filtered,
+    )?;
     Ok(df)
 }
