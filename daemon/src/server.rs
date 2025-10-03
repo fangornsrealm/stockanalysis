4082695713dfd44797dec6bc6c5bbdbf6f73c1bc
fangornsrealm <progs@fangornsrealm.eu>
2025-10-03 17:16:52 +0200

    Implement Functionality and tests for the server

--- daemon/src/server.rs
+++ daemon/src/server.rs
@@ -1,15 +1,22 @@
 use polars::prelude::*;
 use chrono::{Datelike, NaiveTime, offset::Local, Timelike};
-use eyre::{bail, ensure, Result as EyreResult, WrapErr as _};
+use eyre::Result as EyreResult;
+use std::error::Error;
 use structopt::StructOpt;
 use tokio::{
     sync::broadcast,
     time::{self, Duration}
 };
 
-/// convert a Vec<Vec<>> to Vec<&[]>
-pub fn vecs_to_slices<T>(vecs: &[Vec<T>]) -> Vec<&[T]> {
-    vecs.iter().map(Vec::as_slice).collect()
+use api::prelude::*;
+
+/// convert an OsString (from PathBuf) to a usable String
+pub fn osstr_to_string(osstr: std::ffi::OsString) -> String {
+    match osstr.to_str() {
+        Some(str) => return str.to_string(),
+        None => {}
+    }
+    String::new()
 }
 
 #[derive(Debug, PartialEq, StructOpt)]
@@ -19,6 +26,104 @@ pub struct Options {
     pub help: String,
 }
 
+pub async fn run_screener_process() -> Result<(), Box<dyn Error>> {
+    let filepath = std::path::PathBuf::from("testdata");
+    if !filepath.is_dir() {
+        std::fs::create_dir(filepath.clone())?;
+    }
+    // Screen for Large-Cap NASDAQ Stocks
+    let screener = Screener::builder()
+        .quote_type(QuoteType::Equity)
+        .add_filter(ScreenerFilter::EqStr(
+            ScreenerMetric::Equity(EquityScreener::Exchange),
+            Exchange::NASDAQ.as_ref()
+        ))
+        .sort_by(
+            ScreenerMetric::Equity(EquityScreener::MarketCapIntraday),
+            true
+        )
+        .size(10)
+        .build()
+        .await?;
+
+    let file_name = "screener_overview.html";
+    let path = filepath.clone().join(file_name);
+    let overview = screener.overview().to_html();
+    match overview {
+        Ok(chart) => std::fs::write(&osstr_to_string(path.into_os_string()), &chart).expect("Should be able to write to file"),
+        Err(e) => {
+            log::error!("Failed to get overview for screener: {e}");
+            return Ok(());
+        }
+    }
+
+    let file_name = "screener_metrics.html";
+    let path = filepath.clone().join(file_name);
+    let metrics = screener.metrics().await?.to_html();
+    match metrics {
+        Ok(chart) => std::fs::write(&osstr_to_string(path.into_os_string()), &chart).expect("Should be able to write to file"),
+        Err(e) => {
+            log::error!("Failed to get metrics for screener: {e}");
+            return Ok(());
+        }
+    }
+
+    // Instantiate a Multiple Ticker Object
+    let ticker_symbols = screener.symbols.iter()
+        .map(|x| x.as_str()).collect::<Vec<&str>>();
+
+    let tickers = api::models::tickers::TickersBuilder::new()
+        .tickers(ticker_symbols.clone())
+        .start_date("2025-03-01")
+        .end_date("2025-09-15")
+        .interval(Interval::OneDay)
+        .benchmark_symbol("0H1C")
+        .confidence_level(0.95)
+        .risk_free_rate(0.02)
+        .build();
+
+    // Generate a Single Ticker Report
+    let symbol = ticker_symbols.first().unwrap();
+    let ticker = tickers.clone().get_ticker(symbol).await?;
+    let performance = ticker.report(Some(ReportType::Performance)).await?.to_html();
+    let file_name = "screener_top_performance.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &performance).expect("Should be able to write to file");
+    let financials = ticker.report(Some(ReportType::Financials)).await?.to_html();
+    let file_name = "screener_financials.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &financials).expect("Should be able to write to file");
+    let options = ticker.report(Some(ReportType::Options)).await?.to_html();
+    let file_name = "screener_options.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &options).expect("Should be able to write to file");
+    let news = ticker.report(Some(ReportType::News)).await?.to_html();
+    let file_name = "screescreener_newsner_overview.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &news).expect("Should be able to write to file");
+
+    // Generate a Multiple Ticker Report
+    let report = tickers.report(Some(ReportType::Performance)).await?.to_html();
+    let file_name = "screener_report.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &report).expect("Should be able to write to file");
+
+    // Perform a Portfolio Optimization
+    let portfolio = tickers.optimize(Some(ObjectiveFunction::MaxSharpe), None).await?;
+
+    // Generate a Portfolio Report
+    let portfolioreport = portfolio.report(Some(ReportType::Performance)).await?.to_html();
+    let file_name = "screener_portfolioreport.html";
+    let path = filepath.clone().join(file_name);
+    std::fs::write(&osstr_to_string(path.into_os_string()), &portfolioreport).expect("Should be able to write to file");
+
+    // TODO write a HTML file with links to the written HTML files
+
+    // TODO send it via notification and Apple Push notification
+
+    Ok(())
+}
+
 pub fn run_analysis_on_updated_dataframe(
     sql_connection: std::sync::Arc<std::sync::Mutex<rusqlite::Connection>>, 
     symbols: &Vec<String>
@@ -26,10 +131,11 @@ pub fn run_analysis_on_updated_dataframe(
     let now = Local::now();
     
     for symbol in symbols.iter() {
+        let mut vt = Vec::new();
         let mut vv = Vec::new();
-        let start_time = NaiveTime::from_num_seconds_from_midnight_opt(1*3600, 0).expect("That should never fail!");
-        let end_time = NaiveTime::from_num_seconds_from_midnight_opt(23*3600, 0).expect("That should never fail!");
-        let start_date = now.clone().date_naive().checked_sub_days(chrono::Days::new(14)).unwrap().and_time(start_time);
+        let start_time = NaiveTime::from_num_seconds_from_midnight_opt(0, 0).expect("That should never fail!");
+        let end_time = NaiveTime::from_num_seconds_from_midnight_opt(23*3600 + 59*60, 0).expect("That should never fail!");
+        let start_date = now.clone().date_naive().and_time(start_time);
         let end_date = now.clone().date_naive().and_time(end_time);
         let ohlcv: polars::prelude::DataFrame = match api::data::sql::to_dataframe::ohlcv_to_dataframe(
             sql_connection.clone(),
@@ -42,6 +148,13 @@ pub fn run_analysis_on_updated_dataframe(
                     continue;
                 }
                 let mut df = vec[0].clone();
+                match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&df) {
+                    Ok(tv) => vt.push(tv),
+                    Err(error) => {
+                        log::error!("Unable to turn get column timestamp! {:?}", error);
+                        continue;
+                    }
+                };
                 match api::data::sql::to_dataframe::f64_column_to_vec(&df, "adjclose") {
                     Ok(av) => vv.push(av),
                     Err(error) => {
@@ -51,7 +164,15 @@ pub fn run_analysis_on_updated_dataframe(
                 };
                 if vec.len() > 1 {
                     for i in 1..vec.len() {
-                        match api::data::sql::to_dataframe::f64_column_to_vec(&vec[i].clone(), "adjclose") {
+                        let dftmp = vec[i].clone();
+                        match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&dftmp) {
+                            Ok(tv) => vt.push(tv),
+                            Err(error) => {
+                                log::error!("Unable to turn get column timestamp! {:?}", error);
+                                continue;
+                            }
+                        };
+                        match api::data::sql::to_dataframe::f64_column_to_vec(&dftmp, "adjclose") {
                             Ok(av) => vv.push(av),
                             Err(error) => {
                                 log::error!("Unable to turn get column adjclose! {:?}", error);
@@ -81,7 +202,7 @@ pub fn run_analysis_on_updated_dataframe(
                 continue;
             }
         };
-        let datetimes = match api::data::sql::to_dataframe::i64_column_to_datetime_vec(ohlcv.clone()) {
+        let datetimes = match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&ohlcv) {
             Ok(df) => df,
             Err(error) => {
                 log::error!("Unable to turn timestamps into dates! {:?}", error);
@@ -95,9 +216,6 @@ pub fn run_analysis_on_updated_dataframe(
                 continue;
             }
         };
-        let seasonality = api::analytics::detectors::seasonality(&adjclose, 10, 9600, 0.2, false);
-        let changepoints = api::analytics::detectors::changepoints(&adjclose, true);
-        
         
         let jumps = api::analytics::detectors::jumps_in_series(symbol, &timestamps, &adjclose, 0.5, 0.3);
         api::data::sql::events::insert_jump_events(sql_connection.clone(), &jumps);
@@ -105,8 +223,139 @@ pub fn run_analysis_on_updated_dataframe(
         let slope = api::analytics::detectors::increasing_slope(&vv[vv.len()-1], 0.5, 0.3);
         if slope != 0.0 {
             // send alarm
-            log::warn!("Symbol {} jumped by {} at {}!", symbol, slope, datetimes[datetimes.len()-1].to_string());
+            let text;
+            if slope > 0.0 {
+                text = format!("Symbol {} increased by {} at {}!", symbol, slope, datetimes[datetimes.len()-1].to_string());
+            } else {
+                text = format!("Symbol {} dropped by {} at {}!", symbol, slope, datetimes[datetimes.len()-1].to_string());
+            }
+            log::warn!("{}", &text);
+            match notify_rust::Notification::new()
+                .summary("stock-analysis")
+                .body(&text)
+                .icon("alarm")
+                .show()
+            {
+                Ok(_h) => {},
+                Err(e) => log::error!("Failed to notify the desktop user: {}", e),
+            }
         }
+    }
+}
+
+pub fn run_analysis_on_historical_data(
+    sql_connection: std::sync::Arc<std::sync::Mutex<rusqlite::Connection>>, 
+    symbols: &Vec<String>
+) {
+    let now = Local::now();
+    
+    for symbol in symbols.iter() {
+        let mut vt = Vec::new();
+        let mut vv = Vec::new();
+        let start_time = NaiveTime::from_num_seconds_from_midnight_opt(0, 0).expect("That should never fail!");
+        let end_time = NaiveTime::from_num_seconds_from_midnight_opt(23*3600 + 59*60, 0).expect("That should never fail!");
+        let start_date = now.clone().date_naive().checked_sub_days(chrono::Days::new(90)).unwrap().and_time(start_time);
+        let end_date = now.clone().date_naive().and_time(end_time);
+        let ohlcv: polars::prelude::DataFrame = match api::data::sql::to_dataframe::ohlcv_to_dataframe(
+            sql_connection.clone(),
+            symbol,
+            start_date,
+            end_date,
+        ) {
+            Ok(vec) => {
+                if vec.len() == 0 {
+                    continue;
+                }
+                let mut df = vec[0].clone();
+                match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&df) {
+                    Ok(tv) => vt.push(tv),
+                    Err(error) => {
+                        log::error!("Unable to turn get column timestamp! {:?}", error);
+                        continue;
+                    }
+                };
+                match api::data::sql::to_dataframe::f64_column_to_vec(&df, "adjclose") {
+                    Ok(av) => vv.push(av),
+                    Err(error) => {
+                        log::error!("Unable to turn get column adjclose! {:?}", error);
+                        continue;
+                    }
+                };
+                if vec.len() > 1 {
+                    for i in 1..vec.len() {
+                        let dftmp = vec[i].clone();
+                        match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&dftmp) {
+                            Ok(tv) => vt.push(tv),
+                            Err(error) => {
+                                log::error!("Unable to turn get column timestamp! {:?}", error);
+                                continue;
+                            }
+                        };
+                        match api::data::sql::to_dataframe::f64_column_to_vec(&dftmp, "adjclose") {
+                            Ok(av) => vv.push(av),
+                            Err(error) => {
+                                log::error!("Unable to turn get column adjclose! {:?}", error);
+                                continue;
+                            }
+                        };
+                        df = concat([df.lazy(), vec[i].clone().lazy()], UnionArgs::default()).unwrap().collect().unwrap();
+                    }
+                }
+                if df.height() > 0 {
+                    df
+                } else {
+                    // no entries in database or symbol not found, search yahoo instead
+                    continue;
+                }
+
+            },
+            Err(e) => {
+                log::error!("Failed to get dataframe from database for symbol {}: {}", symbol, e);
+                continue;
+            }
+        };
+        let timestamps = match api::data::sql::to_dataframe::i64_column_to_vec(&ohlcv, "timestamp") {
+            Ok(df) => df,
+            Err(error) => {
+                log::error!("Unable to turn get column timestamp! {:?}", error);
+                continue;
+            }
+        };
+        let datetimes = match api::data::sql::to_dataframe::i64_column_to_datetime_vec(&ohlcv) {
+            Ok(df) => df,
+            Err(error) => {
+                log::error!("Unable to turn timestamps into dates! {:?}", error);
+                continue;
+            }
+        };
+        let adjclose = match api::data::sql::to_dataframe::f64_column_to_vec(&ohlcv, "adjclose") {
+            Ok(df) => df,
+            Err(error) => {
+                log::error!("Unable to turn get column adjclose! {:?}", error);
+                continue;
+            }
+        };
+        // start with a series split per business day
+        api::analytics::detectors::cluster_seasonal_data(api::analytics::detectors::vecs_to_slices(&vv));
+        let outliers = api::analytics::detectors::outliers(api::analytics::detectors::vecs_to_slices(&vv));
+        if outliers.len() > 0 {
+            // analyze outliers to find critical events
+        }
+
+        let seasonality = api::analytics::detectors::seasonality(&adjclose, 10, 9600, 0.2, false);
+        for season_length in seasonality {
+            let s = api::analytics::detectors::split_series_into_seasons(&adjclose, season_length as i64, 1);
+            let outliers = api::analytics::detectors::outliers(api::analytics::detectors::vecs_to_slices(&vv));
+        }
+        
+        let changepoints = api::analytics::detectors::changepoints(&adjclose, true);
+        for changepoint in changepoints {
+            // analyze changepoints
+        }
+
+        let jumps = api::analytics::detectors::jumps_in_series(symbol, &timestamps, &adjclose, 0.5, 0.3);
+        api::data::sql::events::insert_jump_events(sql_connection.clone(), &jumps);
+        
     }
 }
 
@@ -118,10 +367,10 @@ pub async fn run_jobs() -> EyreResult<()> {
         // run daily jobs.
         api::data::livedata::update_nightly(sql_connection.clone(), &symbols);
         // temporarily get the minutely data also once per day until there is a subscription with live-data access
+        let start_time = NaiveTime::from_num_seconds_from_midnight_opt(7*3600, 0).expect("That should never fail!");
+        let end_time = NaiveTime::from_num_seconds_from_midnight_opt(22*3600, 0).expect("That should never fail!");
         for symbol in symbols.iter() {
             let mut metadata: api::data::sql::MetaData = api::data::sql::metadata(sql_connection.clone(), "XFRA", symbol);
-            let start_time = NaiveTime::from_num_seconds_from_midnight_opt(7*3600, 0).expect("That should never fail!");
-            let end_time = NaiveTime::from_num_seconds_from_midnight_opt(22*3600, 0).expect("That should never fail!");
             let start_date = now.clone().date_naive().and_time(start_time);
             let end_date = now.clone().date_naive().and_time(end_time);
             metadata.start_date = start_date.clone().and_utc();
@@ -140,6 +389,10 @@ pub async fn run_jobs() -> EyreResult<()> {
                 },
             }
         }
+        run_analysis_on_historical_data(sql_connection.clone(), &symbols);
+
+        let _ret = run_screener_process();
+
     } else {
         // run live updates every minute on Weekdays
         if now.weekday().num_days_from_monday() < 5 {
@@ -148,6 +401,8 @@ pub async fn run_jobs() -> EyreResult<()> {
 
                 // triger the live analysis and event detection
                 run_analysis_on_updated_dataframe(sql_connection.clone(), &symbols);
+
+                
             }
         }
     }
@@ -179,27 +434,33 @@ mod test {
     use pretty_assertions::assert_eq;
 
     #[tokio::test]
-    async fn test_hello_world() {
-        assert_eq!(1, 1);
-    }
-}
-
-#[cfg(feature = "bench")]
-#[allow(clippy::wildcard_imports, unused_imports)]
-pub mod bench {
-    use super::*;
-    use crate::bench::runtime;
-    use criterion::{black_box, Criterion};
-    use hyper::body::to_bytes;
-
-    pub fn group(c: &mut Criterion) {
-        bench_hello_world(c);
+    async fn test_analysis_on_updated_frames() {
+        let sql_connection = api::data::sql::connect();
+        let symbols = api::data::sql::symbols::active_symbols(sql_connection.clone());
+        run_analysis_on_updated_dataframe(sql_connection.clone(), &symbols);
     }
 
-    fn bench_hello_world(c: &mut Criterion) {
-        c.bench_function("bench_hello_world", |b| {
-            b.to_async(runtime()).iter(|| async {
-            });
-        });
+    #[tokio::test]
+    async fn test_analysis_on_historical_data() {
+        let sql_connection = api::data::sql::connect();
+        let symbols = api::data::sql::symbols::active_symbols(sql_connection.clone());
+        run_analysis_on_historical_data(sql_connection.clone(), &symbols);
     }
+
+    #[tokio::test]
+    async fn test_screener() {
+        match run_screener_process().await {
+            Ok(()) => {},
+            Err(e) => log::error!("screener process threw error: {}", e),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_run_jobs() {
+        match run_jobs().await {
+            Ok(()) => {},
+            Err(e) => log::error!("screener process threw error: {}", e),
+        }
+    }
+
 }
